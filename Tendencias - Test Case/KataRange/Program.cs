b44c67e1392e3fb9e17b089dac8cf605ff87ba70
AlfredPolanco <39679348+AlfredPolanco@users.noreply.github.com>
2020-06-09 01:58:01 -0400

    Update Program.cs

--- Tendencias - Test Case/KataRange/Program.cs
+++ Tendencias - Test Case/KataRange/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Buffers;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -10,44 +10,6 @@ using System.Xml.Schema;
 
 namespace KataRange
 {
-    /*
-
-ContainsRange
-
-[2,10) contains [3,5]
-
-[3,5] contains [3,5)
-
-endPoints
-[2,6) endPoints = {2,5}
-
-[2,6] endPoints = {2,6}
-
-(2,6) endPoints = {3,5}
-
-(2,6] endPoints = {3,6}
-
-overlapsRange
-[2,5) doesn’t overlap with [7,10)
-
-[2,10) overlaps with [3,5)
-
-[3,5) overlaps with [3,5)
-
-[2,5) overlaps with [3,10)
-
-[3,5) overlaps with [2,10)
-
-Equals
-[3,5) equals [3,5)
-
-[2,10) neq [3,5)
-
-[2,5) neq [3,10)
-
-[3,5) neq [2,10)
-*/
-
     class Program
     {
         static void Main(string[] args)
@@ -56,63 +18,49 @@ Equals
             string[] separator = { "[", "(", ",", ")", "]", "{", "}" };
             var range = new Range();
 
-            //[2,10) contains [3,5]
-            var containerZero = range.ContainsRange("[2,10)", "[3,5]");
+            //[2,7) contains {3,5}
+            var containerZero = range.ContainsRange("[2,7)", "[3,5]");
             Console.WriteLine(containerZero);
-            //[2,6) contains {2,4}
-            var containerOne = range.ContainsRange("[2,6)", "{2,4}");
+            //[2,6) contains {6,6}
+            var containerOne = range.ContainsRange("[2,7)", "{6,6}");
             Console.WriteLine(containerOne);
-            //[2,6) doesn’t contain {-1,1,6,10}
-            var containerTwo = range.ContainsRange("[2,6)", "{-1,1,6,10}");
+            //[2,4) doesn’t contain {7, 1 ,5}
+            var containerTwo = range.ContainsRange("[2,4)", "{7,1,5}");
             Console.WriteLine(containerTwo);
-            //[2,5) doesn’t contain [7,10)
-            var containerThree = range.ContainsRange("[2,5)", "[7,10)");
+            //[2,4) doesn’t contain [10,-2,3)
+            var containerThree = range.ContainsRange("[2,4)", "{10,-2,7}");
             Console.WriteLine(containerThree);
-            //[2,5) doesn’t contain [3,10)
-            var containerFour = range.ContainsRange("[2,5)", "[3,10)");
-            Console.WriteLine(containerFour);
-            //[3,5) doesn’t contain [2,10)
-            var containerFive = range.ContainsRange("[3,5)", "[2,10)");
-            Console.WriteLine(containerFive);
 
 
-            //[2,6) endPoints = {2,5}
-            var endPointOne = range.endPoints("[2,6)");
-            Console.WriteLine("[2,6) endPoints = {2,5}:");
+            //[5,11] endPoints = [5,11]
+            var endPointOne = range.endPoints("[5,11]");
+            Console.WriteLine("[5,11] endPoints = [5,11]:");
             foreach (var num in endPointOne)
             {
                 Console.Write(num + " ");
             }
-            //[2,6] endPoints = {2,6}
-            var endPointTwo = range.endPoints("[2,6]");
+            //[5,11] endPoints = [7,10]
+            var endPointTwo = range.endPoints("[5,11]");
             Console.WriteLine("");
-            Console.WriteLine("[2,6] endPoints = {2,6}:");
+            Console.WriteLine("[5,11] endPoints = [7,10]:");
             foreach (var num in endPointTwo)
             {
                 Console.Write(num + " ");
             }
-            //(2,6) endPoints = {3,5}
-            var endPointThree = range.endPoints("(2,6)");
+
+            //[2,10) allPoints = {23456789}
+            var allPoints = range.getAllPoints("[2,10)");
             Console.WriteLine("");
-            Console.WriteLine("(2,6) endPoints = {3,5}:");
-            foreach (var num in endPointThree)
-            {
-                Console.Write(num + " ");
-            }
-            //(2,6] endPoints = {3,6}
-            var endPointFour = range.endPoints("(2,6]");
-            Console.WriteLine("");
-            Console.WriteLine("(2,6] endPoints = {3,6}:");
-            foreach (var num in endPointFour)
-            {
-                Console.Write(num + " ");
-            }
-            //[2,6) allPoints = {2,3,4,5}
-            var allPoints = range.getAllPoints("[2,6)");
-            Console.WriteLine("");
-            Console.WriteLine("[2,6) allPoints = {2,3,4,5}:");
+            Console.WriteLine("[2,10) allPoints = {2,10}:");
             foreach (var num in allPoints)
             Console.Write(num + " ");
+
+            //[2,20) allPoints = {23456789}
+            var allPointsOne = range.getAllPoints("[2,20)");
+            Console.WriteLine("");
+            Console.WriteLine("[2,20) allPoints = {2,20}:");
+            foreach (var num in allPointsOne)
+                Console.Write(num + " ");
 
             //[2,10) overlapsRange[1,8)
             var overlapsRange = range.overlapsRange("[2,10)","[1,8)");
@@ -139,16 +87,17 @@ Equals
             Console.WriteLine(equalsRange);
 
             //[10,25] notEquals [10,22]
-            var notEquals = range.notequals("[10,25]", "[10,22]");
+            var notEquals = range.notEquals("[10,25]", "[10,22]");
             Console.WriteLine("");
             Console.WriteLine("[10,25] notEquals = [10,25]");
             Console.WriteLine(notEquals);
 
             //[10,25] notEquals [10,22]
-            var notEqualsOne = range.notequals("[10,25]", "[10,25)");
+            var notEqualsOne = range.notEquals("[10,25]", "[10,25)");
             Console.WriteLine("");
             Console.WriteLine("[10,25] notEquals = [10,25)");
             Console.WriteLine(notEquals);
+
         }
 
     }
