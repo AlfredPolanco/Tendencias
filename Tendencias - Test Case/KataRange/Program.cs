9a8b05a86cac581f2d5110324216e9e6afdb8bf3
AlfredPolanco <39679348+AlfredPolanco@users.noreply.github.com>
2020-06-09 01:28:30 -0400

    Add files via upload

---
+++ Tendencias - Test Case/KataRange/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Xml.Linq;
+using System.Xml.Schema;
+
+namespace KataRange
+{
+    /*
+
+ContainsRange
+
+[2,10) contains [3,5]
+
+[3,5] contains [3,5)
+
+endPoints
+[2,6) endPoints = {2,5}
+
+[2,6] endPoints = {2,6}
+
+(2,6) endPoints = {3,5}
+
+(2,6] endPoints = {3,6}
+
+overlapsRange
+[2,5) doesn’t overlap with [7,10)
+
+[2,10) overlaps with [3,5)
+
+[3,5) overlaps with [3,5)
+
+[2,5) overlaps with [3,10)
+
+[3,5) overlaps with [2,10)
+
+Equals
+[3,5) equals [3,5)
+
+[2,10) neq [3,5)
+
+[2,5) neq [3,10)
+
+[3,5) neq [2,10)
+*/
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] separator = { "[", "(", ",", ")", "]", "{", "}" };
+            var range = new Range();
+
+            //[2,10) contains [3,5]
+            var containerZero = range.ContainsRange("[2,10)", "[3,5]");
+            Console.WriteLine(containerZero);
+            //[2,6) contains {2,4}
+            var containerOne = range.ContainsRange("[2,6)", "{2,4}");
+            Console.WriteLine(containerOne);
+            //[2,6) doesn’t contain {-1,1,6,10}
+            var containerTwo = range.ContainsRange("[2,6)", "{-1,1,6,10}");
+            Console.WriteLine(containerTwo);
+            //[2,5) doesn’t contain [7,10)
+            var containerThree = range.ContainsRange("[2,5)", "[7,10)");
+            Console.WriteLine(containerThree);
+            //[2,5) doesn’t contain [3,10)
+            var containerFour = range.ContainsRange("[2,5)", "[3,10)");
+            Console.WriteLine(containerFour);
+            //[3,5) doesn’t contain [2,10)
+            var containerFive = range.ContainsRange("[3,5)", "[2,10)");
+            Console.WriteLine(containerFive);
+
+
+            //[2,6) endPoints = {2,5}
+            var endPointOne = range.endPoints("[2,6)");
+            Console.WriteLine("[2,6) endPoints = {2,5}:");
+            foreach (var num in endPointOne)
+            {
+                Console.Write(num + " ");
+            }
+            //[2,6] endPoints = {2,6}
+            var endPointTwo = range.endPoints("[2,6]");
+            Console.WriteLine("");
+            Console.WriteLine("[2,6] endPoints = {2,6}:");
+            foreach (var num in endPointTwo)
+            {
+                Console.Write(num + " ");
+            }
+            //(2,6) endPoints = {3,5}
+            var endPointThree = range.endPoints("(2,6)");
+            Console.WriteLine("");
+            Console.WriteLine("(2,6) endPoints = {3,5}:");
+            foreach (var num in endPointThree)
+            {
+                Console.Write(num + " ");
+            }
+            //(2,6] endPoints = {3,6}
+            var endPointFour = range.endPoints("(2,6]");
+            Console.WriteLine("");
+            Console.WriteLine("(2,6] endPoints = {3,6}:");
+            foreach (var num in endPointFour)
+            {
+                Console.Write(num + " ");
+            }
+            //[2,6) allPoints = {2,3,4,5}
+            var allPoints = range.getAllPoints("[2,6)");
+            Console.WriteLine("");
+            Console.WriteLine("[2,6) allPoints = {2,3,4,5}:");
+            foreach (var num in allPoints)
+            Console.Write(num + " ");
+
+            //[2,10) overlapsRange[1,8)
+            var overlapsRange = range.overlapsRange("[2,10)","[1,8)");
+            Console.WriteLine("");
+            Console.WriteLine("[2,10) overlasRange = [1,8)");
+            Console.WriteLine(overlapsRange);
+
+            //[2,10) overlapsRange[1,5)
+            var overlapsRangeOne = range.overlapsRange("[2,10)", "[7,5)");
+            Console.WriteLine("");
+            Console.WriteLine("[2,10) overlasRange = [1,5)");
+            Console.WriteLine(overlapsRange);
+
+            //[5,11) Equals [5,11)
+            var equalsRange = range.equals("[5,11)", "[5,11)");
+            Console.WriteLine("");
+            Console.WriteLine("[5,11) equals = [5,11)");
+            Console.WriteLine(equalsRange);
+
+            //[5,11) Equals [5,11)
+            var equalsRangeOne = range.equals("[5,11]", "[5,11]");
+            Console.WriteLine("");
+            Console.WriteLine("[5,11] equals = [5,11]");
+            Console.WriteLine(equalsRange);
+
+            //[10,25] notEquals [10,22]
+            var notEquals = range.notequals("[10,25]", "[10,22]");
+            Console.WriteLine("");
+            Console.WriteLine("[10,25] notEquals = [10,25]");
+            Console.WriteLine(notEquals);
+
+            //[10,25] notEquals [10,22]
+            var notEqualsOne = range.notequals("[10,25]", "[10,25)");
+            Console.WriteLine("");
+            Console.WriteLine("[10,25] notEquals = [10,25)");
+            Console.WriteLine(notEquals);
+
+
+        }
+
+    }
+}
---
+++ Tendencias - Test Case/KataRange/Range.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KataRange
+{
+    class Range
+    {
+        public string[] separator = { "[", "(", ",", ")", "]", "{", "}" };
+        public bool ContainsRange(string range, string containedRange)
+        {
+            string[] arrRange = range.Split(this.separator,
+                                            StringSplitOptions.RemoveEmptyEntries);
+            string[] arrContainedRange = containedRange.Split(this.separator,
+                                                            StringSplitOptions.RemoveEmptyEntries);
+
+            if (range.Contains(')'))
+            {
+                arrRange[arrRange.Length - 1] = (Convert.ToInt32(arrRange[arrRange.Length - 1]) - 1).ToString();
+            }
+            for (int i = 0; i < arrRange.Length; i++)
+            {
+                for (int j = 0; j < arrContainedRange.Length; j++)
+                {
+                    if (i == 0)
+                    {
+                        if (Convert.ToInt32(arrRange[i]) <= Convert.ToInt32(arrContainedRange[j]))
+                            continue;
+
+                    }
+                    else
+                    {
+                        if (Convert.ToInt32(arrRange[i]) >= Convert.ToInt32(arrContainedRange[j]))
+                            if (j == arrContainedRange.Length - 1 && i == arrRange.Length - 1)
+                                return true;
+                    }
+                }
+            }
+            return false; // If range does not contain containedRange
+        }
+
+        public int[] getAllPoints(string range)
+        {
+            string[] arrRange = range.Split(this.separator,
+                                                      StringSplitOptions.RemoveEmptyEntries);
+
+            int[] arr = new int[Convert.ToInt32(arrRange[arrRange.Length - 1]) - Convert.ToInt32(arrRange[0])];
+
+            if (range.Contains("("))
+                arrRange[0] = (Convert.ToInt32(arrRange[0]) + 1).ToString();
+            if (range.Contains(")"))
+                arrRange[arrRange.Length - 1] = (Convert.ToInt32(arrRange[arrRange.Length - 1]) - 1).ToString();
+
+
+            for (int i = Convert.ToInt32(arrRange[0]), j = 0; i <= Convert.ToInt32(arrRange[arrRange.Length - 1]); i++)
+            {
+                arr[j] = i;
+                j++;
+            }
+
+            return arr;
+        }
+
+        public string[] endPoints(string range)
+        {
+            string[] arrRange = range.Split(this.separator,
+                                                   StringSplitOptions.RemoveEmptyEntries);
+
+            if (range.Contains("("))
+                arrRange[0] = (Convert.ToInt32(arrRange[0]) + 1).ToString();
+            if (range.Contains(")"))
+                arrRange[arrRange.Length - 1] = (Convert.ToInt32(arrRange[arrRange.Length - 1]) - 1).ToString();
+
+            return arrRange;
+        }
+
+        public bool overlapsRange(string range, string comparedRange)
+        {
+
+            string[] arrRange = range.Split(this.separator,
+                                                   StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var num in comparedRange)
+            {
+                if (range.Contains(num))
+                {
+                    return true;
+                }
+            }
+            return false;
+
+        }
+
+        public bool equals(string range, string compared)
+        {
+
+            string[] arrRange = range.Split(this.separator,
+                                                   StringSplitOptions.RemoveEmptyEntries);
+            if(range == compared)
+            {
+                return true;
+            }
+            return false;
+           
+        }
+
+        public bool notequals(string range, string compared)
+        {
+
+            string[] arrRange = range.Split(this.separator,
+                                                   StringSplitOptions.RemoveEmptyEntries);
+            if (range != compared)
+            {
+                return true;
+            }
+            return false;
+
+        }
+
+    }
+}
